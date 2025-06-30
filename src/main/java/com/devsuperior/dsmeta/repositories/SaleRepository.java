2181ee745801df16abc167cdec811eb84fb3c9c0
Júlio Cesar Melegari <juliocesar.melegari@gmail.com>
2025-06-30 14:04:24 -0300

    add countQuery in method search1

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -20,7 +20,15 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 		    WHERE (TB_SELLER.NAME IS NULL OR UPPER(TB_SELLER.NAME) LIKE UPPER(:name))
 		      AND TB_SALES.DATE >= :minDate
 		      AND TB_SALES.DATE <= :maxDate
-		    """)
+		    """,
+		    countQuery = """
+	        SELECT COUNT(*)
+	        FROM TB_SALES
+	        LEFT JOIN TB_SELLER ON TB_SELLER.ID = TB_SALES.SELLER_ID
+	        WHERE (TB_SELLER.NAME IS NULL OR UPPER(TB_SELLER.NAME) LIKE UPPER(:name))
+	          AND TB_SALES.DATE >= :minDate
+	          AND TB_SALES.DATE <= :maxDate
+	        """)
 		Page<SaleWithSellerNameProjection> search1(String name, LocalDate minDate, LocalDate maxDate, Pageable pageable);
 	
 	@Query(nativeQuery = true, 
